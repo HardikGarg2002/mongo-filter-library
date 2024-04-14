e14ad7e7146627272c6544a5aa66d3145efa99b8
Hardik Garg <hardikgarg3085@gmail.com>
2024-04-14 22:14:04 +0530

    testing function

--- src/filter.js
+++ src/filter.js
@@ -1,1 +1,11 @@
 // A function to convert input filter from req of get api request to filter object required for mongoose filter queries
+
+export const convertFilterToMongo = (req) => {
+  const filter = {};
+  for (const key in req.query) {
+    if (req.query[key] !== "") {
+      filter[key] = req.query[key];
+    }
+  }
+  return filter;
+};
